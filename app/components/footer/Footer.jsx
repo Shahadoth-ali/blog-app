773056da270416344bd811556cbd213b6abf36c1
Shahadoth-ali <md.sahadat.live@gmail.com>
2023-09-15 05:13:20 +0600

    sixth commit

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      this is footer
+
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt='lama blog' 
+          width={50} height={50}/>
+          <h1 className={styles.logoText}>lamablog</h1>
+        </div>
+        <p className={styles.desc}>Ut ut no labore kasd sed tempor sit at aliquyam nonumy,
+           vero duo et gubergren duo dolor magna diam, sadipscing.
+           Ipsum vero sadipscing dolor aliquyam invidunt est eirmod
+            tempor et,.</p>
+            <div className={styles.icons}>
+              <Image src="/facebook.png" alt='' width={18} height={18}/>
+              <Image src="/instagram.png" alt='' width={18} height={18}/>
+              <Image src="/tiktok.png" alt='' width={18} height={18}/>
+              <Image src="/youtube.png" alt='' width={18} height={18}/>
+            </div>
+      </div>
+
+      <div className={styles.links}>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+
+
+      </div>
     </div>
   )
 }
